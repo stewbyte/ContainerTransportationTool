11a5f347d5f9edf42d138840dfb2d09c53c0df31
Steffew <xgenyofficial@gmail.com>
2024-03-08 21:38:36 +0100

    Initial commit

---
+++ Container.cs
@@ -0,0 +1,10 @@
+﻿using static ContainerTransportationTool.Enums;
+
+namespace ContainerTransportationTool
+{
+    public class Container
+    {
+        public int weight { get; set; }
+        public ContainerType ContainerType { get; set; }
+    }
+}
---
+++ Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace ContainerTransportationTool
+{
+    public class Enums
+    {
+        public enum ContainerType
+        {
+            Normal,
+            Valueble,
+            Coolable,
+            ValuableAndCoolable
+        }
+    }
+}
