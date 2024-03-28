253ed92a85653da668356ef1309c6679ebe24793
Steffew <xgenyofficial@gmail.com>
2024-03-28 10:53:10 +0100

    Create Ship.cs

---
+++ Ship.cs
@@ -0,0 +1,16 @@
+﻿namespace ContainerTransportationTool
+{
+    internal class Ship
+    {
+        public int stackLength { get; private set; }
+        public int stackWidth { get; private set; }
+        public List<Container> containers { get; private set; }
+        
+        public Ship(int stackLength, int stackWidth)
+        {
+            this.stackLength = stackLength;
+            this.stackWidth = stackWidth;
+            containers = new List<Container>();
+        }
+    }
+}
