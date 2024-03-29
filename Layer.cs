be0c5a3cbdc9521b214f38ee5d3085b609319962
Steffew <xgenyofficial@gmail.com>
2024-03-29 23:25:10 +0100

    Create Layer.cs

---
+++ Layer.cs
@@ -0,0 +1,26 @@
+﻿namespace ContainerTransportationTool
+{
+    internal class Layer
+    {
+        private Container[,] containers;
+
+        public Layer(int length, int width)
+        {
+            containers = new Container[length, width];
+        }
+
+        public void AddContainer(Container container, int x, int y)
+        {
+            if (containers[x, y] != null)
+            {
+                throw new InvalidOperationException("Position is already occupied!");
+            }
+            else if (container == null)
+            {
+                throw new InvalidOperationException("Container is null!");
+            }
+
+            containers[x, y] = container;
+        }
+    }
+}
