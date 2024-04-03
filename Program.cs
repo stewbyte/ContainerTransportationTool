f78d65b4585e9b358624ead7fe0bb05caf8d7c02
Steffew <xgenyofficial@gmail.com>
2024-04-03 20:08:42 +0200

    Add GUI menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ContainerTransportationTool;
+
+bool exit = false;
+Validator validator = new Validator();
+
+while (!exit)
+{
+    Console.WriteLine("// ContainerTransportationTool");
+    Console.WriteLine("1. Create new ship");
+    Console.WriteLine("0. Exit");
+    Console.Write("Enter your choice: ");
+
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Console.Clear();
+            Console.Write("Enter the length of the ship: ");
+            int length = int.Parse(Console.ReadLine());
+            validator.Validate(length, 1, 10);
+
+            Console.Clear();
+            Console.Write("Enter the width of the ship: ");
+            int width = int.Parse(Console.ReadLine());
+            validator.Validate(width, 1, 10);
+            break;
+
+        case "0":
+            exit = true;
+            Console.WriteLine("Exiting...");
+            break;
+
+        default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+    }
+    Console.WriteLine("\nPress any key to continue...");
+    Console.ReadKey();
+    Console.Clear();
+}
