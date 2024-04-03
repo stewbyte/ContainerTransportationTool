552645f4565c62dc60c44e4cc8937e43b070c59d
Steffew <xgenyofficial@gmail.com>
2024-04-03 23:00:34 +0200

    Include Tests library

---
+++ ContainerTransportationTool.Tests/LayerTests.cs
@@ -0,0 +1,34 @@
+namespace ContainerTransportationTool.Tests
+{
+    public class LayerTests
+    {
+        [Fact]
+        public void AddContainerToLayer()
+        {
+            // Arrange
+            Layer layer = new Layer(1, 1);
+            Container container = new Container(Enums.ContainerType.Normal, 4000);
+
+            // Act
+            layer.AddContainer(container, 0, 0);
+
+            // Assert
+            Assert.Equal(container, layer.ReturnContainers()[0, 0]);
+        }
+
+        [Fact]
+        public void AddContainerToOccupiedPosition()
+        {
+            // Arrange
+            Layer layer = new Layer(1, 1);
+            Container container1 = new Container(Enums.ContainerType.Normal, 4000);
+            Container container2 = new Container(Enums.ContainerType.Normal, 4000);
+
+            // Act
+            layer.AddContainer(container1, 0, 0);
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() => layer.AddContainer(container2, 0, 0));
+        }
+    }
+}
