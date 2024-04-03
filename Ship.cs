5955eff4249b182a84222cd52229a92d6b7fa4f6
Steffew <xgenyofficial@gmail.com>
2024-04-03 22:10:07 +0200

    Add layers list to ship class

--- Ship.cs
+++ Ship.cs
@@ -4,11 +4,33 @@
     {
         public int stackLength { get; private set; }
         public int stackWidth { get; private set; }
+        public List<Layer> layers;
 
         public Ship(int stackLength, int stackWidth)
         {
             this.stackLength = stackLength;
             this.stackWidth = stackWidth;
+            layers = new List<Layer>();
+        }
+
+        public void AddLayer(Layer layer)
+        {
+            layers.Add(layer);
+        }
+
+        public void RemoveLayer(int index)
+        {
+            layers.RemoveAt(index);
+        }
+
+        public Layer GetLayer(int index)
+        {
+            return layers[index];
+        }
+
+        public void ClearLayers()
+        {
+            layers.Clear();
         }
     }
 }
