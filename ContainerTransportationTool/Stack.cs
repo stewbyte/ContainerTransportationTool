ebbc2ada6f447813e4eca525cf77095ee90cb6ba
Steffew <xgenyofficial@gmail.com>
2024-05-27 20:47:37 +0200

    Create Stack.cs

---
+++ ContainerTransportationTool/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContainerTransportationTool
+{
+    public class Stack
+    {
+        private List<Container> containers;
+
+        public Stack()
+        {
+            containers = new List<Container>();
+        }
+
+        public void AddContainer(Container container)
+        {
+            containers.Add(container);
+        }
+
+        public Container GetTopContainer()
+        {
+            if (containers.Count == 0)
+            {
+                throw new InvalidOperationException("No container at this position!");
+            }
+
+            return containers[containers.Count - 1];
+        }
+
+        public void DeleteTopContainer()
+        {
+            if (containers.Count == 0)
+            {
+                throw new InvalidOperationException("No container to remove!");
+            }
+
+            containers.RemoveAt(containers.Count - 1);
+        }
+    }
+}
