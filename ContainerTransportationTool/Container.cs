39362dbb23f3aca51c31b823340eb6e80add72cf
Steffew <xgenyofficial@gmail.com>
2024-05-31 11:02:05 +0200

    Optimise container class

--- ContainerTransportationTool/Container.cs
+++ ContainerTransportationTool/Container.cs
@@ -4,10 +4,10 @@ namespace ContainerTransportationTool
 {
     public class Container
     {
-        public int Weight { get; private set; } = 4000;
+        public int Weight { get; private set; }
         public ContainerType ContainerType { get; private set; }
 
-        public Container(ContainerType containerType, int weight)
+        public Container(ContainerType containerType, int weight = 4000)
         {
             Weight = weight;
             ContainerType = containerType;
