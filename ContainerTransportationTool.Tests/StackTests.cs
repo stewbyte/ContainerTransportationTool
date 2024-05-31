055bd52b299e90c494237584e3af9645526fa9fb
Steffew <xgenyofficial@gmail.com>
2024-05-31 23:33:54 +0200

    Implement AddContainer_ShouldThrowExceptionWhenExceedingStackWeightLimit

--- ContainerTransportationTool.Tests/StackTests.cs
+++ ContainerTransportationTool.Tests/StackTests.cs
@@ -20,11 +20,28 @@ namespace ContainerTransportationTool.Tests
 
 
         [Fact]
-        public void AddContainer_ShouldThrowExceptionWhenExceedingWeightLimit()
+        public void AddContainer_ShouldThrowExceptionWhenExceedingContainerWeightLimit()
         {
             // Arrange
             Stack stack = new Stack();
             Container heavyContainer = new Container(ContainerType.Normal, 40);
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => stack.AddContainer(heavyContainer));
+        }
+
+        [Fact]
+        public void AddContainer_ShouldThrowExceptionWhenExceedingStackWeightLimit()
+        {
+            // Arrange
+            Stack stack = new Stack();
+            stack.AddContainer(new Container(ContainerType.Normal, 30));
+            stack.AddContainer(new Container(ContainerType.Normal, 30));
+            stack.AddContainer(new Container(ContainerType.Normal, 30));
+            stack.AddContainer(new Container(ContainerType.Normal, 30));
+            stack.AddContainer(new Container(ContainerType.Normal, 30));
+
+            Container heavyContainer = new Container(ContainerType.Normal, 30);
 
             // Act & Assert
             Assert.Throws<InvalidOperationException>(() => stack.AddContainer(heavyContainer));
