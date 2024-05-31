5dee4dc701603c66555065794eec79ae62ac3840
Steffew <xgenyofficial@gmail.com>
2024-05-31 10:27:26 +0200

    Change name of public variable

--- ContainerTransportationTool/Container.cs
+++ ContainerTransportationTool/Container.cs
@@ -4,12 +4,12 @@ namespace ContainerTransportationTool
 {
     public class Container
     {
-        public int weight { get; private set; }
+        public int Weight { get; private set; }
         public ContainerType ContainerType { get; private set; }
 
         public Container(ContainerType containerType, int weight)
         {
-            this.weight = weight;
+            Weight = weight;
             ContainerType = containerType;
         }
     }
