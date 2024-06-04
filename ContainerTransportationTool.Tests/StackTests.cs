845b408a14c735e2dfdf381b4213cd23815e6ed3
Steffew <xgenyofficial@gmail.com>
2024-06-04 13:38:47 +0200

    Add GetTopContainer tests

--- ContainerTransportationTool.Tests/StackTests.cs
+++ ContainerTransportationTool.Tests/StackTests.cs
@@ -46,5 +46,32 @@ namespace ContainerTransportationTool.Tests
             // Act & Assert
             Assert.Throws<InvalidOperationException>(() => stack.AddContainer(heavyContainer));
         }
+
+        [Fact]
+        public void GetTopContainer_ShouldReturnTopContainer()
+        {
+            // Arrange
+            Stack stack = new Stack();
+            Container container1 = new Container(ContainerType.Normal, 10);
+            Container container2 = new Container(ContainerType.Normal, 20);
+            stack.AddContainer(container1);
+            stack.AddContainer(container2);
+
+            // Act
+            Container topContainer = stack.GetTopContainer();
+
+            // Assert
+            Assert.Equal(container2, topContainer);
+        }
+
+        [Fact]
+        public void GetTopContainer_ShouldThrowExceptionWhenStackIsEmpty()
+        {
+            // Arrange
+            Stack stack = new Stack();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => stack.GetTopContainer());
+        }
     }
 }
